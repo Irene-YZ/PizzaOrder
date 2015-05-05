e2bd3ad90649cc93becc08da41fe4826ca1d824a
Irene <irene55hang@gmail.com>
2015-05-05 01:13:04 -0700

    Pizza Order Sample
    Pizza Order Sample

---
+++ Pizza Order Mobile/js/Script2.js
@@ -0,0 +1,23 @@
+
+var pizza1 = new Object();
+pizza1.name = Papperoni;
+pizza1.small = 8.99;
+pizza1.medium = 9.99;
+pizza2.large = 10.99;
+
+
+
+var pizza2 = new Object();
+pizza2.name = Vegetarian;
+pizza2.small = 9.99;
+
+
+pizza2.medium = 10.99;
+pizza2.large = 11.99;
+
+var pizza3 = new Object();
+pizza3.name = Combo;
+pizza3.small = 10.99;
+pizza3.medium = 11.99;
+pizza3.large = 12.99;
+
